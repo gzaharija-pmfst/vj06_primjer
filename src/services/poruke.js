45474c856fecdcd476c4d1db2a1a7758151eafe4
gzaharija-pmfst <71737050+gzaharija-pmfst@users.noreply.github.com>
2021-11-10 17:20:34 +0100

    Promjena adrese URL na Heroku

--- src/services/poruke.js
+++ src/services/poruke.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const osnovniUrl = 'http://localhost:3001/api/poruke'
+//const osnovniUrl = 'http://localhost:3001/api/poruke'
+const osnovniUrl = 'https://vj06-server-gz.herokuapp.com/api/poruke'
  
 const dohvatiSve = () => {   
     return axios.get(osnovniUrl);
